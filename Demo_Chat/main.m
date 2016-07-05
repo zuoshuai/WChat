1323ad1907d28d771b182ab4db470f66d63a1303
Sam <yzlindevelop@163.com>
2016-07-05 22:14:36 +0800

    Initial Commit

---
+++ Demo_Chat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo_Chat
+//
+//  Created by admin on 16/7/5.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
